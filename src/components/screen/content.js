ff57a87c4ffca2e8392e370c6292503621f3d7b2
fredPack <fred.paquereau69@gmail.com>
2021-01-28 16:53:08 +0100

    babylon Front Page

---
+++ src/components/screen/content.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FadeRight from './fade'
+import "./content.css"
+
+  function Content (){    
+  return (   
+    <>
+    <FadeRight />
+    </>
+  );
+}
+
+
+export default Content
